0053f3cad5c0375ea1a0132605fc6f7be9b54931
Akhil <akhil9tiet@gmail.com>
2021-08-05 23:12:17 -0700

    add redux reducer action and store

---
+++ src/redux/reducers/productReducer.js
@@ -0,0 +1,14 @@
+import { ActionTypes } from "../constants/action-types";
+
+const intitialState = {
+	products: [],
+};
+export const productReducer = (state = intitialState, { type, payload }) => {
+	switch (type) {
+		case ActionTypes.SET_PRODUCTS:
+			return state;
+
+		default:
+			return state;
+	}
+};
